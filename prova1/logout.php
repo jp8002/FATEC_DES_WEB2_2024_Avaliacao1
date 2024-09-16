25b4b7a81c9d12ae76c331ff307fa733ff08b487
jp8002 <162762832+jp8002@users.noreply.github.com>
2024-09-16 20:44:35 -0300

    Delete prova1 directory

--- prova1/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	$_POST = array();
-	session_destroy();
-	header("location:index.php")
-
-?>
--- prova1/start.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    if (!isset($_SESSION["auth"]) || $_SESSION['auth'] == FALSE) {
-        header("location:index.php");
-    }
-
-
-?>
