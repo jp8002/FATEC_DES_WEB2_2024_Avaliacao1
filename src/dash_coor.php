2e14929480d06cffae18a19e67d18ef98db5648a
jp8002 <162762832+jp8002@users.noreply.github.com>
2024-09-16 20:45:11 -0300

    Add files via upload

---
+++ src/dash_coor.php
@@ -0,0 +1,17 @@
+<?php
+    require "start.php";
+    require "head.html"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard</title>
+</head>
+<body>
+	<h1>Área da Coordenação</h1>
+	<a href="registrar.php"><button type="button" class="btn btn-primary">Registrar</button></a>
+	<a href="logout.php"><button type="button" class="btn btn-danger">Logout</button></a>
+</body>
+</html>
